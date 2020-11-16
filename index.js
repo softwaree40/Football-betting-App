8c502f1f722494d2f06e82073e6d1dd981e9ee13
softwaree40 <softwaree40@gmail.com>
2020-11-16 17:05:04 -0500

    adding list of players name for each teams in a array

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict"
+
+const game = {
+team1: "Bayern Munich",
+team2: "Borrussia Dortmund",
+players: [
+[ 
+    "Neuer",
+    "Pavard",
+    "Martinez",
+    "Alaba",
+    "Davies",
+    "Kimmich",
+    "Goretzka",
+    "Coman",
+    "Muller",
+    "Gnarby",
+    "Lewa dowski"
+
+
+],
+[
+  "Burki",
+  "Schulz",
+  "Hummels",
+  "Akanji",
+  "Hakimi",
+  "Weigl",
+  "Witsel",
+  "Hazard",
+  "Brandt",
+
+
+]
+
+]
+
+
+
+
+
+}
